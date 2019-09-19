b13c9e16afcb6446c48ee8aba435639b5908d342
github username <github email address>
2019-09-18 18:21:40 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,54 @@
+// const dogName;
+// const dogBreed;
+
+
+function wakeDog(dogName, dogBreed) {
+    const sentence = `Wake ${dogName} the ${dogBreed}`
+    console.log(sentence)
+    return sentence
+}
+
+function leashDog(dogName, dogBreed) {
+    const sentence = `Leash ${dogName} the ${dogBreed}`
+    console.log(sentence)
+    return sentence
+}
+
+function walkToPark(dogName, dogBreed) {
+    const sentence = `Walk to the park with ${dogName} the ${dogBreed}`
+    console.log(sentence)
+    return sentence
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    const sentence = `Throw the frisbee for ${dogName} the ${dogBreed}`
+    console.log(sentence)
+    return sentence
+}
+
+function walkHome(dogName, dogBreed) {
+    const sentence = `Walk home with ${dogName} the ${dogBreed}`
+    console.log(sentence)
+    return sentence
+}
+
+function unleashDog(dogName, dogBreed) {
+    const sentence = `Unleash ${dogName} the ${dogBreed}`
+    console.log(sentence)
+    return sentence
+}
+
+const routine = [
+    wakeDog,
+    leashDog,
+    walkToPark,
+    throwFrisbee,
+    walkHome,
+    unleashDog
+]
+
+
+
+function exerciseDog(dogName, dogBreed) {
+    return routine.map(action => action(dogName, dogBreed))
+}
